000b0fd01e9271e07441f72b34326d4d055cfcc2
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-08-21 14:54:18 +0200

    change environment variable name

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -8,7 +8,7 @@ from ports.ServerProtocol import ServerAddress
 
 if __name__ == "__main__":
     device_server_port = os.getenv("PORT", "8091")
-    server_port = os.getenv("SERVER_PORT", None)
+    server_address = os.getenv("SERVER_ADDRESS", None)
     server_broadcast_port = os.getenv("SERVER_DISCOVERY_PORT",  "30000")
     server_broadcast_host = os.getenv("SERVER_DISCOVERY_ADDR", "255.255.255.255")
     server_broadcast_address = ServerAddress(server_broadcast_host, int(server_broadcast_port))
@@ -24,10 +24,10 @@ if __name__ == "__main__":
         period_sec=1
     )
     app = create_server(washing_machine_agent)
-    if(server_port is not None and server_port != ""):
-        print("Server: ", server_port)
+    if(server_address is not None and server_address != ""):
+        print("Server: ", server_address)
         washing_machine_agent.set_server_address(
-            ServerAddress("localhost", int(server_port))
+            ServerAddress(server_address)
         )
     washing_machine_agent.start()
     uvicorn.run(app, host="0.0.0.0", port=int(device_server_port))
